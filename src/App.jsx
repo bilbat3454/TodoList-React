48ac0e9869904b243b9f3ced340b49129d21c82a
bilal <bilalbates@gmail.com>
2024-07-24 15:41:08 -0400

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
 function App() {  
-  let todos = [
-    'Go to the gym',
-    'Eat more fruits and veggies',
-    'Pick up the kids from school'
-]
+  const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState('')
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index) {
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
+  }
 
   return (
     <>
-      <TodoInput />
-      <TodoList todos={todos}/>
+      <TodoInput todoValue={todoValue} setTodoValue=
+      {setTodoValue} handleAddTodos={handleAddTodos} />
+      <TodoList handleEditTodo={handleEditTodo} 
+      handleDeleteTodo={handleDeleteTodo} todos={todos}/>
     </>
   )
 }
